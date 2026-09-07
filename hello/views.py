49beb74b790e3a194bebff9990c1ff6643aa823e
Daria Burenko <da.renoook@gmail.com>
2026-09-07 21:34:44 +0300

    Changed text and color

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1 style="color:green;">Hello from the first app</h1>')
+    return HttpResponse('<h1 style="color:purple; font-size: 36px;">never give up</h1>')
 
 
 def fancy_index(request):
